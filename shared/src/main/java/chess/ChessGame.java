c05223108aa5edf32eaea2a6e3f1d59a8552880e
beefstew2002 <stuarteliason@hotmail.com>
2025-01-29 15:25:17 -0700

    Added the folders to prepare for Phase 1 Added a helper function isInDanger to complete isInCheck Completed isInCheck and passed corresponding tests, yippee!

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,15 +11,19 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    private TeamColor whoseTurn;
+    private ChessBoard theBoard;
 
+    public ChessGame() {
+        whoseTurn = TeamColor.WHITE;
+        theBoard = new ChessBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return whoseTurn;
     }
 
     /**
@@ -27,7 +32,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        whoseTurn = team;
     }
 
     /**
@@ -46,6 +51,9 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+        //Get the set of possible moves for the piece
+        //Check to see if any of the moves leave the king in danger
+        //Remove those moves from the list
         throw new RuntimeException("Not implemented");
     }
 
@@ -66,7 +74,30 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        //Create a helper function isInDanger to check if any given piece is in danger
+        //throw new RuntimeException("Not implemented");
+
+        //Identify the king of the team
+        //ChessPiece king;
+        ChessPosition kingPosition = new ChessPosition(0,0);
+        //Scan through every square of the board (feel like I'm doing that a bunch) to get a king of the color
+        //Maybe I'll add an Iterator to ChessBoard
+        ChessPosition targetPosition = new ChessPosition(0,0);
+        ChessPiece targetPiece;
+        for (int r=1; r<=8; r++) {
+            for (int c=1; c<=8; c++) {
+                targetPosition.setCol(c);
+                targetPosition.setRow(r);
+                targetPiece = theBoard.getPiece(targetPosition);
+                if (targetPiece != null && targetPiece.getPieceType() == ChessPiece.PieceType.KING && targetPiece.getTeamColor() == teamColor) {
+                    //king = targetPiece;
+                    kingPosition = targetPosition.copy();
+                }
+            }
+        }
+        if (kingPosition.equals(new ChessPosition(0,0))) {return false;} //for a weird edge case where there is no king
+
+        return isInDanger(kingPosition);
     }
 
     /**
@@ -76,6 +107,8 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
+        //If the king is in check AND the team has no valid moves
+        //A helper function to check if the team has no valid moves would be good
         throw new RuntimeException("Not implemented");
     }
 
@@ -87,6 +120,7 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
+        //If the king is NOT in check AND the team has no valid moves
         throw new RuntimeException("Not implemented");
     }
 
@@ -96,7 +130,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        theBoard = board;
     }
 
     /**
@@ -105,6 +139,38 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return theBoard;
+    }
+
+    //Checks if the piece at a given position is in danger
+    public boolean isInDanger(ChessPosition position) {
+        //Identify the color of the piece at the position
+        TeamColor myTeam = theBoard.getPiece(position).getTeamColor();
+        TeamColor enemyColor;
+        if (myTeam.equals(TeamColor.WHITE)) enemyColor = TeamColor.BLACK;
+        else enemyColor = TeamColor.WHITE;
+
+        //Loop through each of the pieces on the opposite team
+        ChessPosition targetPosition = new ChessPosition(1,1);
+        ChessPiece targetPiece;
+        ArrayList<ChessMove> moves;
+        for (int x=1; x<=8; x++) { for (int y=1; y<=8; y++) {
+            //Look at the current square
+            targetPosition.setRow(y);
+            targetPosition.setCol(x);
+            targetPiece = theBoard.getPiece(targetPosition);
+            if (targetPiece != null && targetPiece.getTeamColor() == enemyColor) {
+                moves = (ArrayList<ChessMove>) targetPiece.pieceMoves(theBoard, targetPosition);
+                for (int i=0; i<moves.size(); i++) {
+                    //If any pieces of the opposite team can move to this square
+                    //Return true
+                    if (moves.get(i).getEndPosition().equals(position)) {
+                        return true;
+                    }
+                }
+            }
+
+        }}
+        return false;
     }
 }
