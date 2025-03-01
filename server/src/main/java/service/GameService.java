9e3e0a444d647fd15d68d5af2ba98cde8f576e3a
beefstew2002 <stuarteliason@hotmail.com>
2025-03-01 10:35:23 -0700

    clear endpoint now passes the success test

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,11 +1,22 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 import service.RequestResult.*;
 
 public class GameService {
     //public static CreateResult create(CreateRequest createRequest) {}
     //public static ListResult list(ListRequest listRequest) {}
     public static ClearResult clear(ClearRequest clearRequest) {
+        UserDAO udao = new UserDAO();
+        AuthDAO adao = new AuthDAO();
+        GameDAO gdao = new GameDAO();
+
+        udao.clearData(); //The way it's written right now, a single clearData will erase everything
+        adao.clearData(); //Including the others in case that changes later, I might rewrite the class
+        gdao.clearData();
+
         return new ClearResult();
     }
 }
