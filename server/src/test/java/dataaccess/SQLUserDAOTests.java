2b1f9a7b8f51ef4ae7646678994bb11616244ce9
beefstew2002 <stuarteliason@hotmail.com>
2025-03-12 23:22:51 -0600

    getUserData tests

--- server/src/test/java/dataaccess/SQLUserDAOTests.java
+++ server/src/test/java/dataaccess/SQLUserDAOTests.java
@@ -1,4 +1,147 @@
 package dataaccess;
 
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static dataaccess.DatabaseManager.getConnection;
+
 public class SQLUserDAOTests {
+
+
+    UserDAO udao;
+    AuthDAO adao;
+    GameDAO gdao;
+
+    String username = "peeps";
+    String password = "password";
+    String email = "email@com";
+    UserData user;
+    //setup
+    @BeforeEach
+    public void setup() {
+        udao = new UserDAO();
+        adao = new AuthDAO();
+        gdao = new GameDAO();
+
+        udao.clearData();
+        adao.clearData();
+        gdao.clearData();
+
+        user = new UserData(username, password, email);
+    }
+
+    //invent user
+    private void inventUser() throws DataAccessException {
+
+        inventUser(username, password, email);
+    }
+    private void inventUser(String username, String password, String email) throws DataAccessException {
+
+        //Add auth data
+        try (var conn = getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?);")) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+                preparedStatement.setString(3, email);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private ArrayList<UserData> checkUsers() {
+        ArrayList<UserData> userData = new ArrayList<UserData>();
+
+        try (var conn = getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT * FROM user;")) {
+                try (var rs = preparedStatement.executeQuery()) {
+                    while (rs.next()) {
+                        String username = rs.getString("username");
+                        String password = rs.getString("password");
+                        String email = rs.getString("email");
+                        UserData aUser = new UserData(username, email, password);
+                        userData.add(aUser);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
+        return userData;
+
+    }
+
+
+    //Success getUserData
+    @Test
+    @DisplayName("Success getUserData")
+    public void successgetUserData() throws DataAccessException{
+        inventUser();
+        inventUser("cromslor", "bingus", "poob has it for you");
+
+        Assertions.assertEquals(checkUsers(), udao.getUserData());
+
+    }
+    //Failure getUserData
+    @Test
+    @DisplayName("Failure getUserData")
+    public void failgetUserData() {
+        //No data to get
+        Assertions.assertEquals(new ArrayList<UserData>(), udao.getUserData());
+    }
+
+    //Success getUser
+    @Test
+    @DisplayName("Success getUser")
+    public void successgetUser() {
+
+    }
+    //Failure getUser
+    @Test
+    @DisplayName("Failure getUser")
+    public void failgetUser() {
+
+    }
+
+    //Success createUser
+    @Test
+    @DisplayName("Success createUser")
+    public void successcreateUser() {
+
+    }
+    //Failure createUser
+    @Test
+    @DisplayName("Failure createUser")
+    public void failcreateUser() {
+
+    }
+
+    //Success isEmpty
+    @Test
+    @DisplayName("Success isEmpty")
+    public void successisEmpty() {
+
+    }
+    //Failure isEmpty
+    @Test
+    @DisplayName("Failure isEmpty")
+    public void failisEmpty() {
+
+    }
+
+    //Success clearData
+    @Test
+    @DisplayName("Success clearData")
+    public void successclearData() {
+
+    }
+
 }
