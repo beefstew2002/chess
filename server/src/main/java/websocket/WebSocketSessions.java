7b2d4273bedd6c8f8f055f7968ae118162299353
beefstew2002 <stuarteliason@hotmail.com>
2025-04-08 23:05:32 -0600

    Created WebSocketHandler and WebSocketSessions. Don't revert past this point

---
+++ server/src/main/java/websocket/WebSocketSessions.java
@@ -0,0 +1,28 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class WebSocketSessions {
+    private Map<Integer, Set<Session>> sessionMap;
+
+    public WebSocketSessions() {
+        sessionMap = new HashMap<Integer, Set<Session>>();
+    }
+
+    public void addSessionToGame(int gameId, Session session) {
+        sessionMap.get(gameId).add(session);
+    }
+
+    public void removeSessionFromGame(int gameId, Session session) {
+        sessionMap.get(gameId).remove(session);
+    }
+
+    public Set<Session> getSessionsFromGame(int gameId) {
+        return sessionMap.get(gameId);
+    }
+}
+
